67ffbfd4e254f703be55b3882c7f9c294c49fc5e
aiunam <cotraduccion@gmail.com>
2024-01-25 16:48:16 +0900

    Added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
-
 
 //https://docs.astro.build/en/guides/deploy/github/
 // 1.여기 깃헙 업로드위해서 수정 site 라인 있는줄 2.Configure a GitHub Action보고 파일 생성
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
+  adapter: vercel()
 });
