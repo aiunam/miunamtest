c2590e8f90b89c66a429ae2190f13427bad6817e
aiunam <cotraduccion@gmail.com>
2024-01-25 13:55:54 +0900

    upload all files of Miunam

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = 'Miunam Website';
+export const SITE_DESCRIPTION = 'Articles, programs and Tutorials';
+export const HOMEPAGE_ARTICLE_LIMIT = 6;
+export const ARTICLES_PER_PAGE = 6;
